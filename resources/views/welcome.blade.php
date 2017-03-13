8847e6a673292f38e151ce382663ec1d5c42e974
yasuo.mori <shigkleibizz@gmail.com>
2017-03-13 22:09:56 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>ツイッターぽいの</h1>
+            {!! link_to_route('signup.get', '新規登録する', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
